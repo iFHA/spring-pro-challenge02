e359821e866f06cd7049b6798ee0578ba762abc5
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-06 20:55:45 -0300

    bloco

---
+++ src/main/java/dev/fernando/spring_pro_ch02/entities/Bloco.java
@@ -0,0 +1,22 @@
+package dev.fernando.spring_pro_ch02.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant inicio;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant fim;
+}
