e9ddaa227f7de6a375681a83af7caf49df76c882
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-06 21:24:44 -0300

    entidade atividade finalizada

--- src/main/java/dev/fernando/spring_pro_ch02/entities/Atividade.java
+++ src/main/java/dev/fernando/spring_pro_ch02/entities/Atividade.java
@@ -1,6 +1,7 @@
 package dev.fernando.spring_pro_ch02.entities;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -35,4 +36,70 @@ public class Atividade {
     @OneToMany(mappedBy = "atividade")
     private List<Bloco> blocos = new ArrayList<>();
 
+    public Atividade() {}
+
+    public Atividade(Integer id, String nome, String descricao, Double preco, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Set<Participante> getParticipantes() {
+        return Collections.unmodifiableSet(participantes);
+    }
+
+    public void addParticipante(Participante participante) {
+        this.participantes.add(participante);
+    }
+
+    public List<Bloco> getBlocos() {
+        return Collections.unmodifiableList(blocos);
+    }
+
+    public void addBloco(Bloco bloco) {
+        this.blocos.add(bloco);
+    }
+    
 }
