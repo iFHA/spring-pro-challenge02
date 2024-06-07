7b9bafd7034e544eb63b9e0e99060ec1a772aedc
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-06 21:21:13 -0300

    entidade bloco finalizada

--- src/main/java/dev/fernando/spring_pro_ch02/entities/Bloco.java
+++ src/main/java/dev/fernando/spring_pro_ch02/entities/Bloco.java
@@ -25,4 +25,71 @@ public class Bloco {
     @ManyToOne
     @JoinColumn(name = "atividade_id")
     private Atividade atividade;
+
+    public Bloco() {}
+
+    public Bloco(Integer id, Instant inicio, Instant fim, Atividade atividade) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fim = fim;
+        this.atividade = atividade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
+
+    public Atividade getAtividade() {
+        return atividade;
+    }
+
+    public void setAtividade(Atividade atividade) {
+        this.atividade = atividade;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Bloco other = (Bloco) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    
 }
