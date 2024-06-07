c2b2ceba7e43c4e821f4bcf4f92ea53b483bfc0c
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-06 21:19:54 -0300

    restante dos relacionamentos

--- src/main/java/dev/fernando/spring_pro_ch02/entities/Atividade.java
+++ src/main/java/dev/fernando/spring_pro_ch02/entities/Atividade.java
@@ -1,9 +1,18 @@
 package dev.fernando.spring_pro_ch02.entities;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,4 +24,15 @@ public class Atividade {
     private String nome;
     private String descricao;
     private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    @ManyToMany(mappedBy = "atividades")
+    private Set<Participante> participantes = new HashSet<>();
+
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
+
 }
