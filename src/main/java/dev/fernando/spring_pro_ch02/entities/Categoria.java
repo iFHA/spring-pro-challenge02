56d6f48eb7b2d90c9f245962d35a543a5cf24680
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-06 21:16:06 -0300

    construtores, getters e setters de categoria

--- src/main/java/dev/fernando/spring_pro_ch02/entities/Categoria.java
+++ src/main/java/dev/fernando/spring_pro_ch02/entities/Categoria.java
@@ -1,6 +1,7 @@
 package dev.fernando.spring_pro_ch02.entities;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import jakarta.persistence.Entity;
@@ -20,4 +21,61 @@ public class Categoria {
 
     @OneToMany(mappedBy = "categoria")
     private List<Atividade> atividades = new ArrayList<>();
+
+    public Categoria() {}
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Atividade> getAtividades() {
+        return Collections.unmodifiableList(atividades);
+    }
+
+    public void addAtividade(Atividade atividade) {
+        this.atividades.add(atividade);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Categoria other = (Categoria) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    
 }
